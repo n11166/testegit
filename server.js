7c40d2a823207f430feaa41522e1ded762ff8bad
n11166 <77644118+n11166@users.noreply.github.com>
2021-01-18 19:34:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.status(200).send({ message: 'Olá mundo estou funcionando!' });
 })
 
+app.get('/ola', (req, res) => {
+  res.status(200).send({ message: 'Oi ... eu sou a rota Ola!' });
+})
+
 app.listen(3001, () => {
   console.log('API rodando na porta 3001');
 })
